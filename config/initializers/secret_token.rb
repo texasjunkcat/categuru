fe71ee29eab08281d53f607679698a68e5224646
Tim Graney <timgraney@gmail.com>
2014-02-03 23:02:47 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Categuru::Application.config.secret_key_base = 'b07e1300bab6834124c940c87a72fdf501ed2bed7653df1d78c4685960240e6dd42c41fd98f6dead48ef0e68d5b3234c6b4227681f22a3c0f88965cbf555a2f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Categuru::Application.config.session_store :cookie_store, key: '_categuru_session'
